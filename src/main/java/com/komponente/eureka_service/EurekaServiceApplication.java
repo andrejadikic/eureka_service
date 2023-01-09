f62c47000baec89ef0d485e7a978ea73a5a9a514
andrejadikic <86166465+andrejadikic@users.noreply.github.com>
2023-01-09 18:42:59 +0100

    Initial commit

---
+++ src/main/java/com/komponente/eureka_service/EurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.komponente.eureka_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
